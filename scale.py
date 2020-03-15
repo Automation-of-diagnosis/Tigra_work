99008cc3cf9aaebc3d619e59e54fff508f88d0ac
Tigran <tigrava86@gmail.com>
2020-03-15 16:53:59 +0300

    New

---
+++ scale.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    try:
+        title='Оценка тяжести состояния пациента'
+        scale='Введите показатели:'
+        return render_template ('index.html', page_title=title, scale=scale)
+    except (IndexError, TypeError):
+        return False
+
+
+if __name__=='__main__':
+    app.run(debug=True)
